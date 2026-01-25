54fa32a60cd7beae4f78419956ea7b48481a0af2
AkshatJha21 <akshatj2134@gmail.com>
2026-01-26 00:08:44 +0530

    component

--- frontend/src/pages/landing/landing_page.tsx
+++ frontend/src/pages/landing/landing_page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/common/navbar'
 
 const LandingPage = () => {
   return (
     <div>
       Forever App Landing
+      <Navbar />
     </div>
   )
 }
