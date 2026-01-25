82214e965b4957f385922e371969e35df3b4cf33
AkshatJha21 <akshatj2134@gmail.com>
2026-01-25 23:25:36 +0530

    Add base pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter,  Route, Routes } from "react-router-dom";
-import TestComponent from "./components/test";
+import LandingPage from "./pages/landing/landing_page";
+import LoginPage from "./pages/auth/login_page";
+import SignupPage from "./pages/auth/signup_page";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<TestComponent />} path="/"/>
+        <Route element={<LandingPage />} path="/"/>
+        <Route element={<LoginPage />} path="/login"/>
+        <Route element={<SignupPage />} path="/signup"/>
       </Routes>
     </BrowserRouter>
   )
