808d0c8b67b17c9bd61b04ee55ff611023ce5d42
AkshatJha21 <akshatj2134@gmail.com>
2026-02-04 23:46:50 +0530

    Add create note function to sidebar

--- frontend/src/components/common/sidebar.tsx
+++ frontend/src/components/common/sidebar.tsx
@@ -1,19 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import AllNotes from "./all_notes"
 
-const Sidebar = () => {
+interface SidebarProps {
+    onCreate: () => void;
+}
+
+const Sidebar = ({ onCreate }: SidebarProps) => {
     const navigate = useNavigate();
     const handleLogout = () => {
         localStorage.removeItem("token");
         navigate('/login');
-    }
+    };
   return (
     <div className="h-full w-68.75 bg-slate-50 flex flex-col px-2">
         <div className="w-full flex px-2 py-4 mb-2 items-center border-b border-slate-200">
             <p className="flex-1 font-black text-xl text-blue-900">4ever</p>
             <button onClick={handleLogout} className="border border-slate-200 px-2 py-1 text-sm rounded hover:bg-slate-200 cursor-pointer transition-all">Logout</button>
         </div>
-        <button className="px-4 py-2 m-2 border font-medium border-slate-200 rounded text-sm hover:bg-slate-200 cursor-pointer transition-all">+ New Note</button>
+        <button onClick={onCreate} className="px-4 py-2 m-2 border font-medium border-slate-200 rounded text-sm hover:bg-slate-200 cursor-pointer transition-all">+ New Note</button>
         <button className="px-4 py-2 m-2 border font-medium border-slate-200 rounded text-sm hover:bg-slate-200 cursor-pointer transition-all">Note of the Day</button>
         <div className="m-2 ">
             <p className="flex-1 font-bold text-blue-900 text-lg">Your Notes</p>
--- frontend/src/pages/dashboard/dashboard.tsx
+++ frontend/src/pages/dashboard/dashboard.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import Sidebar from '../../components/common/sidebar';
+import CreateNotes from '../../components/common/create_notes';
 
 const Dashboard = () => {
+  const [openCreate, setOpenCreate] = useState(false);
   return (
     <div className='h-screen w-full flex'>
-        <Sidebar />
+        <Sidebar onCreate={() => setOpenCreate(true)}/>
+        {openCreate && (
+          <CreateNotes />
+        )}
     </div>
   )
 }
