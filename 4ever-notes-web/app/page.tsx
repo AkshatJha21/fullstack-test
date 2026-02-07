476da1b67be3a1f6e4fed5cfa6bbb6617dcc9ae9
AkshatJha21 <akshatj2134@gmail.com>
2026-02-07 13:28:49 +0530

    Setup next js

---
+++ 4ever-notes-web/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+      <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">4ever Notes</h1>
+      <Button>Test</Button>
+    </main>
+  );
+}
